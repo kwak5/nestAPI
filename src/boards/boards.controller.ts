5d4e01997083fdd16e679cf566fdd64a67549135
seong woo kwak <rickiq2011@naver.com>
2024-01-29 04:49:24 +0900

    메모리 저장/ 간단한 CRUD

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
-@Controller('boards')
-export class BoardsController {}
+@Controller('/boards')
+export class BoardsController {
+    constructor(private boardsService:BoardsService){}
+
+    @Get('/')
+    getAllBoards(){
+        return this.boardsService.findAllBoards();
+    }
+
+    @Post('/')
+    createBoard(
+        @Body('title') title:string,
+        @Body('description') description:string
+        ){
+        return this.boardsService.createBoard(title,description);
+    }
+
+    @Get('/:id')
+    findBoardById(@Param('id') id : string){
+        return this.boardsService.findById(id);
+    }
+
+    @Patch('/:id')
+    findBoardByIdAndUpdate(
+        @Param('id') id:string,
+        @Body('title') title?:string,
+        @Body('description') description?:string
+        ){
+            return this.boardsService.findByIDAndUpdate(id,title,description);
+        }
+
+    @Delete('/:id')
+    deleteBoard(@Param('id') id:string){
+        this.boardsService.deleteBoard(id)
+    }
+}
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './board';
+import {v1 as uuid} from 'uuid'
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    private boardRepository : Board[] = [];
+
+    createBoard(title:string,description:string):Board{
+        const board : Board = {
+            id:uuid(),
+            title,
+            description,
+        }
+        this.boardRepository.push(board)
+        return board;
+    }
+
+    findById(id:string):Board{
+        return this.boardRepository.find((b)=>b.id===id)
+        
+    }
+
+    findAllBoards():Board[]{
+        return this.boardRepository;
+    }
+
+    findByIDAndUpdate(id:string,title?:string,description?:string):Board{
+        const board : Board = this.findById(id);
+        board.title = title;
+        board.description = description;
+        return board;
+    }
+    deleteBoard(id:string):void{
+        this.boardRepository = this.boardRepository.filter((b)=>b.id !== id);
+    }
+
+}
 
